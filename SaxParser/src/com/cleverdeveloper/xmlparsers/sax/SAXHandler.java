5284b85b4401317fd8610cae4a5cbb0b1f60a4df
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-02 23:15:22 +0100

    Implementing the Handler methods ...

--- SaxParser/src/com/cleverdeveloper/xmlparsers/sax/SAXHandler.java
+++ SaxParser/src/com/cleverdeveloper/xmlparsers/sax/SAXHandler.java
@@ -7,22 +7,45 @@ Author Name : @ DRRONIDZ
 DATE : 1/2/2022 11:04 PM
 */
 
+import com.cleverdeveloper.xmlparsers.dto.DriversLicense;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class SAXHandler extends DefaultHandler {
 
+    private DriversLicense driversLicense;
+    private String content;
+
     @Override
     public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+        if (qName.equals("DriversLicence")) {
+            driversLicense = new DriversLicense();
+        }
+
 
     }
 
     @Override
     public void endElement(String uri, String localName, String qName) throws SAXException {
+
+        switch (qName) {
+
+            case "Number":
+                driversLicense.setNumber(Long.parseLong(content));
+                break;
+            case "FirstName":
+                driversLicense.setFirstName(content);
+                break;
+            case "LastName":
+                driversLicense.setLastName(content);
+                break;
+        }
     }
 
     @Override
     public void characters(char[] ch, int start, int length) throws SAXException {
+        content = String.copyValueOf(ch, start, length).trim();
     }
 }
