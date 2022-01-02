577e38d7ca412b43a4e33f6f3526a692097fd8df
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-02 22:44:40 +0100

    Reading attribute values & formatting the files ...

--- XMLParser/src/com/cleverdeveloper/xmlparsers/dom/DomParserDemo.java
+++ XMLParser/src/com/cleverdeveloper/xmlparsers/dom/DomParserDemo.java
@@ -9,6 +9,7 @@ DATE : 1/2/2022 10:22 PM
 
 import com.cleverdeveloper.xmlparsers.dto.DriversLicense;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
@@ -27,6 +28,8 @@ public class DomParserDemo {
                 .getClassLoader().getSystemResourceAsStream("com/xml/DriversLicense.xml"));
         DriversLicense driversLicense = new DriversLicense();
 
+        /* Reading the elements by name */
+                                    /*              - BEGIN -              */
         NodeList numberList = document.getElementsByTagName("Number");
         Node numberItem = numberList.item(0);
         driversLicense.setNumber(Long.parseLong(numberItem.getTextContent()));
@@ -38,7 +41,17 @@ public class DomParserDemo {
         NodeList lastNameList = document.getElementsByTagName("LastName");
         Node lastNameItem = lastNameList.item(0);
         driversLicense.setLastName(lastNameItem.getTextContent());
+                                    /*              -  END -              */
+        /* Reading the attribute values */
+                                    /*              - BEGIN -              */
+        Element documentElement = document.getDocumentElement();
+        driversLicense.setStatus(documentElement.getAttribute("status"));
+                                    /*              -  END -              */
 
+
+        System.out.println("####################### Displaying Information #######################");
+
+        System.out.println("Status is : " + driversLicense.getStatus());
         System.out.println("Number is : " + driversLicense.getNumber());
         System.out.println("First name is : " + driversLicense.getFirstName());
         System.out.println("Last name is : " + driversLicense.getLastName());
