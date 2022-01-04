32433be47b499ff98542410434c38ec340aea1b3
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-04 22:50:56 +0100

    2- Handling the startElements & Text DATA ...

--- STAXParser/src/com/cleverdeveloper/staxparser/main/STAXParserMain.java
+++ STAXParser/src/com/cleverdeveloper/staxparser/main/STAXParserMain.java
@@ -7,7 +7,10 @@ Author Name : @ DRRONIDZ
 DATE : 1/4/2022 10:40 PM
 */
 
+import com.cleverdeveloper.staxparser.dto.DriversLicense;
+
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
@@ -16,5 +19,24 @@ public class STAXParserMain {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLStreamReader xmlStreamReader = factory.createXMLStreamReader(
                 ClassLoader.getSystemResourceAsStream("com/cleverdeveloper/xml/DriversLicense.xml"));
+
+        DriversLicense driversLicense = null;
+        String content = null;
+
+        while (xmlStreamReader.hasNext()) {
+            int event = xmlStreamReader.next();
+
+            switch (event) {
+                case XMLStreamConstants.START_ELEMENT:
+                    if(xmlStreamReader.getLocalName().equals("DriverLicence")) {
+                        driversLicense = new DriversLicense();
+                    }
+                    break;
+
+                case XMLStreamConstants.CHARACTERS:
+                    content = xmlStreamReader.getText();
+                    break;
+            }
+        }
     }
 }
