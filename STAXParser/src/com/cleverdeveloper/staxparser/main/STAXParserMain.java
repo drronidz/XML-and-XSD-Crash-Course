15b562f73411b5cb34b50780ca4b6379b62d818f
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-01-04 23:10:36 +0100

    3- Handling the attributes values ...

--- STAXParser/src/com/cleverdeveloper/staxparser/main/STAXParserMain.java
+++ STAXParser/src/com/cleverdeveloper/staxparser/main/STAXParserMain.java
@@ -30,6 +30,16 @@ public class STAXParserMain {
                 case XMLStreamConstants.START_ELEMENT:
                     if (xmlStreamReader.getLocalName().equals("DriversLicense")) {
                         driversLicense = new DriversLicense();
+
+                        for (int i = 0; i < xmlStreamReader.getAttributeCount(); i++) {
+
+                            switch (xmlStreamReader.getAttributeLocalName(i)) {
+
+                                case "status":
+                                    driversLicense.setStatus(xmlStreamReader.getAttributeValue(i));
+                                    break;
+                            }
+                        }
                     }
                     break;
 
@@ -52,6 +62,7 @@ public class STAXParserMain {
                     }
             }
         }
+        System.out.println(driversLicense.getStatus());
         System.out.println(driversLicense.getNumber());
         System.out.println(driversLicense.getFirstName());
         System.out.println(driversLicense.getLastName());
